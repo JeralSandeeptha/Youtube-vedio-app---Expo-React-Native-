57b0dc6d9c9fdb3ccd01d3379ac57535f38d2ad8
Jeral <jeral.sandeeptha1@gmail.com>
2023-02-19 18:37:24 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import YoutubePlayer from "react-native-youtube-iframe";
+import {useState, useCallback, useRef} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [playing, setPlaying] = useState(false);
+
+    const onStateChange = useCallback((state) => {
+        if (state === "ended") {
+            setPlaying(false);
+            Alert.alert("video has finished playing!");
+        }
+    }, []);
+
+    const togglePlaying = useCallback(() => {
+        setPlaying((prev) => !prev);
+    }, []);
+
+      return (
+        <View style={styles.container}>
+            <StatusBar style='light'/>
+            <View style={styles.a}></View>
+            <YoutubePlayer
+                height={300}
+                width={'100%'}
+                play={playing}
+                videoId={"cNgL40UfhbA"}
+                onChangeState={onStateChange}
+            />
+            {/* <Button title={playing ? "pause" : "play"} onPress={togglePlaying} /> */}
+        </View>
+      );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  a: {
+    marginTop: 29
+  }
 });
